ae8cf7fd748f1f334085a29a0e4cd789b5c844ce
robbicode <robbiaziisworld@gmail.com>
2023-05-17 20:58:54 +0700

    day 3

---
+++ day_2/day_2.py
@@ -0,0 +1,23 @@
+fruits = ['banana', 'orange', 'mango', 'lemon']
+jumlah = len(fruits)
+
+first_fruit = fruits[0]
+print(first_fruit)
+
+second_fruit = fruits[1]
+print(second_fruit)
+
+last_fruit = fruits[3]
+print(last_fruit)
+
+last_fruit = fruits[-1]
+second_last = fruits[-2]
+print(last_fruit)       
+print(second_last)
+
+all_fruits = fruits[0:4] 
+all_fruits = fruits[0:]
+orange_and_mango = fruits[1:3]
+orange_mango_lemon = fruits[1:]
+does_exist = 'banana' in fruits
+print(does_exist)
