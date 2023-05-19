33aeb3152c98bc98abe0dcd3e621c04ee2c4a9d1
robbicode <robbiaziisworld@gmail.com>
2023-05-19 14:33:17 +0700

    day3

---
+++ day_3/day_3.py
@@ -0,0 +1,11 @@
+score = int(input('Input your grade:\n'))
+if score >= 79:
+    print("Your grade is A") 
+elif score >= 69:
+    print("Your grade is B")
+elif score >= 59:
+    print("Your grade is C")
+elif score >= 49:
+    print("Your grade is D")
+else :
+    print("Your grade is F")
